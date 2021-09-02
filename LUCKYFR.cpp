21a48a49756785e586f309e1735eec55179b3abe
Runal Bandishti <42735893+RunalBandishti@users.noreply.github.com>
2021-09-02 17:04:04 +0530

    Create LUCKYFR.cpp

---
+++ LUCKYFR.cpp
@@ -0,0 +1,51 @@
+##Question:
+
+Karan likes the number 4 very much.
+
+Impressed by the power of this number, Karan has begun to look for occurrences of four anywhere. He has a list of T integers, for each of them he wants to calculate the number of occurrences of the digit 4 in the decimal representation. He is too busy now, so please help him.
+
+Input
+The first line of input consists of a single integer T, denoting the number of integers in Karan's list.
+
+Then, there are T lines, each of them contain a single integer from the list.
+
+Output
+Output T lines. Each of these lines should contain the number of occurrences of the digit 4 in the respective integer from Karan's list.
+  
+  
+
+##Solution:
+  
+  
+  
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int T = 0;
+    cin >> T;
+    
+    while(T--)
+    {
+        int no = 0, cnt = 0;
+        cin >> no;
+        
+        while(no != 0)
+        {
+            if((no % 10) == 4)
+                cnt++;
+            no /= 10;
+        }
+        cout << cnt << endl;
+        
+    }
+	return 0;
+}  
+  
+  
+  
+  
+  
+  
+  
